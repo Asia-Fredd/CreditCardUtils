55699d1c7266e31a843d428a89cc191c4990e6e5
FreddY <55474596+Asia-Fredd@users.noreply.github.com>
2019-12-19 02:55:41 +0800

    實作 DiscoverCard 驗證和擷取
    Signed-off-by: FreddY <55474596+Asia-Fredd@users.noreply.github.com>

--- base/src/main/kotlin/asia/fredd/tools/creditcardutils/type/AmericanExpress.kt
+++ base/src/main/kotlin/asia/fredd/tools/creditcardutils/type/AmericanExpress.kt
@@ -12,21 +12,30 @@ class AmericanExpress private constructor(override val cardNumber: CharSequence)
     companion object {
         /**
          * 查核卡號 是否符合 美國運通 信用卡號編碼
+         *
+         * IIN = 34, 37
+         *
          * @return
          * - 不符合 -> null
          * - 符合 -> (非空) CardType/AmericanExpress
+         *
+         * @see
+         * [1] https://en.wikipedia.org/wiki/Payment_card_number
+         * <br>
+         * [2]https://github.com/getbouncer/cardscan-android/blob/1.0.5115/cardscan-base/src/main/java/com/getbouncer/cardscan/base/CreditCardUtils.java#L80
          */
         fun Valid(src: CharSequence): CardType? = when {
             // 先判斷長度是否大於 15
             src.length < 15 -> null
             // 判斷是否符合 IIN = 34 或 37
             src[0] != '3' -> null
-            src[1] != '4' && src[1] != '7' -> null
-            // 最後驗證 Luhn 演算法
-            else -> src
-                .subSequence(0, 15)
-                .takeIf(Luhn.Companion::Check)
-               ?.run(::AmericanExpress)
+            else -> when (src[1]) {
+                '4', '7' -> src.subSequence(0, 15).run(::convert) // 最後驗證 Luhn 演算法
+                else -> null
+            }
         }
+
+        private fun convert(src: CharSequence): CardType? =
+            src.takeIf(Luhn.Companion::Check)?.run(::AmericanExpress)
     }
 }
---
+++ base/src/main/kotlin/asia/fredd/tools/creditcardutils/type/DiscoverCard.kt
@@ -0,0 +1,41 @@
+@file:Suppress("FunctionName")
+
+package asia.fredd.tools.creditcardutils.type
+
+import asia.fredd.tools.creditcardutils.base.CardType
+import asia.fredd.tools.creditcardutils.base.Luhn
+
+/**
+ * 發現卡
+ */
+class DiscoverCard private constructor(override val cardNumber: CharSequence) : CardType {
+    companion object {
+        /**
+         * 查核卡號 是否符合 發現卡 信用卡號編碼
+         *
+         * IIN = 6011, 622126 - 622925, 624000 - 626999, 628200 - 628899, 64, 65
+         *
+         * @return
+         * - 不符合 -> null
+         * - 符合 -> (非空) CardType/DiscoverCard
+         *
+         * @see
+         * [1] https://en.wikipedia.org/wiki/Payment_card_number
+         * <br>
+         * [2] https://github.com/getbouncer/cardscan-android/blob/1.0.5115/cardscan-base/src/main/java/com/getbouncer/cardscan/base/CreditCardUtils.java#L101
+         */
+        fun Valid(src: CharSequence): CardType? = when {
+            // 先判斷長度是否大於 16
+            src.length < 16 -> null
+            // 判斷是否符合 IIN = 6011, 622126 - 622925, 624000 - 626999, 628200 - 628899, 64, 65
+            src[0] != '6' -> null
+            else -> when (src[1]) {
+                '0', '2', '4', '5' -> src.subSequence(0, 16).run(::convert) // 最後驗證 Luhn 演算法; TODO: 未處理卡號長度大於16~19
+                else -> null
+            }
+        }
+
+        private fun convert(src: CharSequence): CardType? =
+            src.takeIf(Luhn.Companion::Check)?.run(::DiscoverCard)
+    }
+}
